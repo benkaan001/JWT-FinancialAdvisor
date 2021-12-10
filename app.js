11c69cdc6ca81be9b89c14c1202629f674a23a14
Ben Kaan <benkaan001@gmail.com>
2021-12-10 17:01:12 -0600

    add error middlewares

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 
 const express = require('express');
-const errorHandlerMiddleware = require('./middleware/error-handler');
 const app = express();
 require('dotenv').config();
 require('express-async-errors');
+
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 
 app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
 
 
 app.get('/', (req,res) => {

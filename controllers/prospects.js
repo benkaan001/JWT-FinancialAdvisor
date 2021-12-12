88c0083f7d2804b3ad95aa41d02a2e39ed359672
Ben Kaan <benkaan001@gmail.com>
2021-12-12 00:51:51 -0600

    finish Prospect routes

--- controllers/prospects.js
+++ controllers/prospects.js
@@ -1,16 +1,29 @@
 const Prospect = require("../models/Prospect");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, NotFoundError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const getAllProspects = async (req, res) => {
   const prospects = await Prospect.find({ createdBy: req.user.userId }).sort(
-    "status")
+    "status"
+  );
   res.status(StatusCodes.OK).json({ prospects });
 };
 
 const getProspect = async (req, res) => {
-  res.send("get single prospect");
+  const {
+    user: { userId },
+    params: { id: prospectId },
+  } = req;
+  const prospect = await Prospect.findOne({
+    _id: prospectId,
+    createdBy: userId,
+  });
+  if (!prospect) {
+    throw new NotFoundError(`No prospect found with this ${prospectId}`);
+  }
+  res.status(StatusCodes.OK).json({ prospect });
 };
+
 const createProspect = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const prospect = await Prospect.create(req.body);
@@ -18,10 +31,44 @@ const createProspect = async (req, res) => {
 };
 
 const updateProspect = async (req, res) => {
-  res.send("update single prospect");
+  const {
+    body: { name, portfolioValue },
+    user: { userId },
+    params: { id: prospectId },
+  } = req;
+
+  if (name === "" || portfolioValue === "") {
+    throw new BadRequestError("Name and Portfolio values are requied!");
+  }
+
+  const prospect = await Prospect.findByIdAndUpdate(
+    {
+      _id: prospectId,
+      createdBy: userId,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!prospect) {
+    throw new NotFoundError(`No prospect found with this ${prospectId}`);
+  }
+  res.status(StatusCodes.OK).json({ prospect });
 };
+
 const deleteProspect = async (req, res) => {
-  res.send("delete single prospect");
+  const {
+    user: { userId },
+    params: { id: prospectId },
+  } = req;
+  const prospect = await Prospect.findByIdAndDelete({
+    _id: prospectId,
+    createdBy: userId,
+  });
+  if (!prospect) {
+    throw new NotFoundError(`No prospect found with this ${prospectId}`);
+  }
+  res.status(StatusCodes.OK).json({ msg: `${prospect.name} successfully deleted!` });
 };
 
 module.exports = {
--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,6 @@
 const NotAuthenticatedError = require('./notAuthenticated');
 const CustomAPIError = require('./custom-error');
 const BadRequestError = require('./bad-request');
+const NotFoundError = require('./not-found');
 
-module.exports = { NotAuthenticatedError, CustomAPIError, BadRequestError}
+module.exports = { NotAuthenticatedError, CustomAPIError, BadRequestError, NotFoundError}

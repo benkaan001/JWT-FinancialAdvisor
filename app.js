81f4c6c147b9ad52770a311e6319dea4cf1f8d84
Ben Kaan <benkaan001@gmail.com>
2021-12-12 14:18:27 -0600

    db connection

--- app.js
+++ app.js
@@ -49,14 +49,13 @@ const port = process.env.PORT || 3000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
-            console.log(`🌍----> Server is listening on port ${port}----->🌍`);
-        })
+        app.listen(port, () => 
+            console.log(`🌍----> Server is listening on port ${port}----->🌍`));
         
     } catch (error) {
         console.log(error);
         
     }
-}
+};
 
 start();
--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const connectDB = (url) => {
     return mongoose.connect(url, {
         useUnifiedTopology: true,
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
     })
 };
 

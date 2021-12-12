7f9bef0265986b3ad78ffcef47dd3513770de099
Ben Kaan <benkaan001@gmail.com>
2021-12-12 16:00:45 -0600

    add swagger api docs

--- app.js
+++ app.js
@@ -1,66 +1,73 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
 // security npm packages
-const cors = require('cors');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const limiter = require('express-rate-limit');
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const limiter = require("express-rate-limit");
 
+// API dependencies
+
+const swagger = require("swagger-ui-express");
+const yaml = require("yamljs");
+const apiDoc = yaml.load("./api.yaml");
 
 // DB connection
-const connectDB = require('./db/connect');
-const isAuthenticated = require('./middleware/authentication')
+const connectDB = require("./db/connect");
+const isAuthenticated = require("./middleware/authentication");
 
 // routers
-const userRoutes = require('./routes/user');
-const prospectsRoutes = require('./routes/prospects')
-
+const userRoutes = require("./routes/user");
+const prospectsRoutes = require("./routes/prospects");
 
 //refer to docs for limit values
-app.set('trust proxy', 1);
-app.use(limiter({
-    windowMs: 15 * 60 * 1000,  // 15 mins
-    max: 100,  // limit each IP to 100 requests per window
-}));
+app.set("trust proxy", 1);
+app.use(
+  limiter({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 100, // limit each IP to 100 requests per window
+  })
+);
 
 app.use(express.json());
-
 
 app.use(cors());
 app.use(helmet());
 app.use(xss());
 
 // routes
-app.use('/api/v1/user', userRoutes )
-app.use('/api/v1/prospects', isAuthenticated, prospectsRoutes)
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/prospects", isAuthenticated, prospectsRoutes);
 
 //dummy route
-app.get('/', (req, res) => {
-    res.send('financialAdvisorAPI');
-  });
+app.get("/", (req, res) => {
+  res.send(
+    '<h2>Financial Advisor API</h2> <a href="/api-docs">Documentation</a>'
+  );
+});
 
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const notFoundMiddleware = require('./middleware/not-found')
+// api global middleware
+app.use("/api-docs", swagger.serve, swagger.setup(apiDoc));
+
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
-
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => 
-            console.log(`🌍----> Server is listening on port ${port}----->🌍`));
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`🌍----> Server is listening on port ${port}----->🌍`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
